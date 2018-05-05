6b2ac0fb43faf69c7f71d60dba311a376f956787
Kevin Yang <chgc_kevin@hotmail.com>
2018-05-05 16:37:46 +0800

    修正環境設定

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,40 +10,49 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace stream_tools
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(options => options.AddPolicy("CorsPolicy",
-             builder =>
-             {
-                 builder.WithOrigins("http://localhost:4200")
-                        .AllowCredentials()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-             }));
+      services.AddCors(options => options.AddPolicy("CorsPolicy",
+       builder =>
+       {
+         builder.WithOrigins("http://localhost:4200")
+                      .AllowCredentials()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+       }));
 
-            services.AddSignalR();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseCors("CorsPolicy");
-            app.UseSignalR(routes =>
-                       {
-                           routes.MapHub<ActionHub>("/actionHub");
-                       });
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+      services.AddSignalR();
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      app.UseCors("CorsPolicy");
+      app.UseSignalR(routes =>
+                 {
+                   routes.MapHub<ActionHub>("/actionHub");
+                 });
+
+      app.Use(async (context, next) =>
+      {
+        await next();
+        if (context.Response.StatusCode == 404 &&
+            !Path.HasExtension(context.Request.Path.Value) &&
+            !context.Request.Path.Value.StartsWith("/api/"))
+        {
+          context.Request.Path = "/index.html";
+          await next();
+        }
+      });
+      app.UseStaticFiles();
+    }
+  }
 }
