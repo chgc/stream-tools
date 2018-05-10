d1ad60a87c3ae8bf2fcb903d37b3bf831c3fb761
Kevin Yang <chgc_kevin@hotmail.com>
2018-05-10 23:57:43 +0800

    新增功能 button 列表

--- client/src/app/guest/guest.component.ts
+++ client/src/app/guest/guest.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { HubService } from '../hub.service';
 import { CommandModel } from '../command.interface';
 import { ToolsService } from '../tools.service';
-
-export const MAX_WIDTH = 1920;
-export const MAX_HEIGHT = 1080;
-export const START_X = 0;
-export const START_Y = 0;
+import { of } from 'rxjs';
+export const MAX_WIDTH = 1620;
+export const MAX_HEIGHT = 980;
+export const START_X = 100;
+export const START_Y = 50;
 
 @Component({
   selector: 'app-guest',
@@ -14,6 +14,26 @@ export const START_Y = 0;
   styleUrls: ['./guest.component.css']
 })
 export class GuestComponent implements OnInit {
+  buttons$ = of([
+    { label: '斗內時間', value: '斗內時間', colorClass: 'btn-danger', style: {} },
+    { label: '哈哈哈', value: '哈哈哈', colorClass: 'btn-warning', style: {} },
+    { label: '咬我啊', value: '咬我啊', colorClass: 'btn-primary', style: {} },
+    { label: '77777777', value: '77777777', colorClass: 'btn-primary', style: { order: 1, color: '#ff0' } },
+    { label: '買！都買！', value: '買！都買！', colorClass: 'btn-primary', style: {} },
+    { label: '作好！作滿！', value: '作好！作滿！', colorClass: 'btn-primary', style: {} },
+    { label: '捐好捐滿', value: '捐好捐滿', colorClass: 'btn-primary', style: {} },
+    { label: '推坑', value: '推坑', colorClass: 'btn-warning', style: {} },
+    { label: '牛排!!', value: '牛排!!', colorClass: 'btn-warning', style: {} },
+    { label: '啊！壞掉了!', value: '啊！壞掉了!', colorClass: 'btn-primary', style: {} },
+    { label: '嗶嗶！犯規！', value: '嗶嗶！犯規！', colorClass: 'btn-primary', style: {} },
+    { label: '欸～真假啦？！', value: '欸～真假啦？！', colorClass: 'btn-primary', style: {} },
+    { label: '人生好累', value: '人生好累', colorClass: 'btn-primary', style: {} },
+    { label: 'GG了', value: 'GG了', colorClass: 'btn-primary', style: {} },
+    { label: '有必要嗎??', value: '有必要嗎??', colorClass: 'btn-primary', style: {} },
+    { label: '幻覺！全都是幻覺', value: '幻覺！全都是幻覺', colorClass: 'btn-primary', style: {} },
+    { label: 'LIVE Demo 魔咒發生了！', value: 'LIVE Demo 魔咒發生了！', colorClass: 'btn-warning', style: {} }
+  ]);
+
   constructor(private service: ToolsService) {}
 
   ngOnInit() {}
