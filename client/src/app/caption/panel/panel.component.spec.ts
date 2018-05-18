f51bc8a48e4bb4253556ebb40282b71865db78f9
Kevin Yang <chgc_kevin@hotmail.com>
2018-05-18 20:32:12 +0800

    add wallaby testing

--- client/src/app/caption/panel/panel.component.spec.ts
+++ client/src/app/caption/panel/panel.component.spec.ts
@@ -1,24 +1,49 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { NgxsModule, Store } from '@ngxs/store';
+import { Subject } from 'rxjs';
+import { AuthService } from '../../auth.service';
+import { ToolsService } from '../services/tools.service';
+import { EnvironmentState } from '../sotre/environment.state';
 import { PanelComponent } from './panel.component';
+import { CaptionItemsState } from '../sotre/caption-items.state';
+import { CaptionService } from '../services/caption.service';
+
+export class FakeAuthService {
+  authState = new Subject();
+}
 
 describe('panelComponent', () => {
   let component: PanelComponent;
   let fixture: ComponentFixture<PanelComponent>;
+  const fakeToolsService = jasmine.createSpyObj('ToolsService', [
+    'init',
+    'joinRoom'
+  ]);
+  const fakeCaptionService = jasmine.createSpyObj('fakeCaptionService', [
+    'initFireStore'
+  ]);
+  let store: Store;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PanelComponent]
+      declarations: [PanelComponent],
+      imports: [NgxsModule.forRoot([EnvironmentState, CaptionItemsState])],
+      providers: [
+        { provide: AuthService, useClass: FakeAuthService },
+        { provide: ToolsService, useValue: fakeToolsService },
+        { provide: CaptionService, useValue: fakeCaptionService }
+      ]
     }).compileComponents();
+    store = TestBed.get(Store);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PanelComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
--- client/src/app/caption/services/caption.service.spec.ts
+++ client/src/app/caption/services/caption.service.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CaptionService } from './caption.service';
+import { AngularFirestore } from 'angularfire2/firestore';
 
+export class FakeAngularFirestore {}
 describe('CaptionService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CaptionService]
+      providers: [CaptionService, { provide: AngularFirestore, useClass: FakeAngularFirestore }]
     });
   });
 
-  it('should be created', inject([CaptionService], (service: CaptionService) => {
-    expect(service).toBeTruthy();
-  }));
+  it(
+    'should be created',
+    inject([CaptionService], (service: CaptionService) => {
+      expect(service).toBeTruthy();
+    })
+  );
 });
