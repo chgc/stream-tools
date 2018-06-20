e3c7b08d51dab2e4eb4ce2c4e4b94411c6ae4c9f
Kevin Yang <chgc_kevin@hotmail.com>
2018-06-20 12:34:57 +0800

    update chat component spect file

--- client/src/app/youtube/chat/chat.component.spec.ts
+++ client/src/app/youtube/chat/chat.component.spec.ts
@@ -1,7 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ChatComponent } from './chat.component';
 import { BroadcastService } from '../broadcast.service';
+import { PrizeDrawService } from '../prize-draw.service';
+import { of } from 'rxjs';
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
@@ -9,13 +10,20 @@ describe('ChatComponent', () => {
   const broadcastServiceSpy = jasmine.createSpyObj('BroadcastService', [
     'selectBroadcastChat',
     'getBroadcastList',
-    'getBroadcastChat'
+    'getBroadcastChat',
+    'startWatchBroadcastChat',
+    'stopWatchBroadcastChat'
   ]);
+
+  const prizeDrawSeriveSpy = jasmine.createSpyObj('PrizeDrawService', ['']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ChatComponent],
-      providers: [{ provide: BroadcastService, useValue: broadcastServiceSpy }]
+      providers: [
+        { provide: BroadcastService, useValue: broadcastServiceSpy },
+        { provide: PrizeDrawService, useValue: prizeDrawSeriveSpy }
+      ]
     }).compileComponents();
   }));
 
@@ -27,5 +35,19 @@ describe('ChatComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should start watchBroadcastChat', () => {
+    broadcastServiceSpy.startWatchBroadcastChat.and.returnValue(of(''));
+    component.setLiveChatId(1, 'test');
+    expect(broadcastServiceSpy.startWatchBroadcastChat).toHaveBeenCalledWith(1);
+    expect(component.isCapturingMessage).toBeTruthy();
+    expect(component.eventTitle).toEqual('test');
+  });
+
+  it('should stop watch broadcast chat', () => {
+    component.stop();
+    expect(broadcastServiceSpy.stopWatchBroadcastChat).toHaveBeenCalled();
+    expect(component.isCapturingMessage).toBeFalsy();
   });
 });
